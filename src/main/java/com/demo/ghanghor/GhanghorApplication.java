c84140e25796241b9efd04411739275e1aa9d769
Chirag Bhimani <chirag.bhimani@zymr.com>
2017-12-08 03:25:25 +0530

    commit every thing

---
+++ src/main/java/com/demo/ghanghor/GhanghorApplication.java
@@ -0,0 +1,31 @@
+package com.example.ghanghor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController 
+public class GhanghorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhanghorApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/events/{data}", method = RequestMethod.GET)
+	public ResponseEntity events(@PathVariable Object data) {
+		System.out.println("Exceted GET method with param.." + data);
+			return ResponseEntity.status(HttpStatus.OK).body(data);
+	}
+		
+	@RequestMapping(value = "/events", method = RequestMethod.GET)
+	public ResponseEntity events2() {
+			System.out.println("Exceted GET method without param..");
+			return ResponseEntity.status(HttpStatus.OK).build();
+	}
+}
